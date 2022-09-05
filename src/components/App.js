2fcd93f012c60a3a3e2014dbcb3c7f282dae3b3a
Suraj Kumar <108967267+9304764919@users.noreply.github.com>
2022-09-05 12:32:29 +0530

    Update App.js
    update attribute

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,8 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-    <h1 data-ns-test attribute="project-name">Facebook</h1>
-    <h2 data-ns-test attribute="project-description"> Social Media clone App</h2>
-    
+    <h1 data-ns-test="project-name">Instagreen</h1>
+    <p data-ns-test="project-description">Instagreen Would be a instagreen clone app which have all the functinality of instagram using firebase as a database</p>
     </div>
   )
 }
